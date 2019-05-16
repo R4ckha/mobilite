fa9a2b82473051af8280a31b751c34cc9b6ce77b
bezounours <gratien.therond@gmail.com>
2019-05-16 11:57:25 +0200

    Update js + html

--- js/form.js
+++ js/form.js
@@ -1,3 +1,40 @@
 window.onload = () => {
-	let blocIsDisplayed = document.querySelector('');
+
+	// Selection du bouton suivant
+	let nextButton = document.querySelector('.next-button'),
+		buttonParent = nextButton.parentElement.parentElement;
+
+	let situation = document.querySelector('#situation');
+
+	// On écoute l'evenement 'click' sur le bouton
+	nextButton.addEventListener('click', function(event){
+		// Puis on inhibe son comportement par defaut 
+		event.preventDefault();
+		// Et on cache le bloc pour passer au suivant
+		buttonParent.style.display = "none";
+		buttonParent.nextElementSibling.style.display = "flex";
+
+	});
+
+	situation.onchange = function(){
+		let options = this.getElementsByTagName("option"),
+			response = options[this.selectedIndex].innerHTML;
+		
+		let validResponses = new Array("Célibataire","Marié","Veuf","Divorcé");
+		if (validResponses.indexOf(response) != -1) {
+			console.log('OK bitch');
+		} else {
+			console.log('NOOOOPE');
+		}
+	}
+	/*
+	// On désactive la fonction initial des liens pour en faire des boutons
+	for (var i = 0; i < buttonCollection.length; i++) {
+		buttonCollection[i].addEventListener('click', function (e) {
+			e.preventDefault();
+			console.log("lien bloqué");
+		});
+	}
+	*/
+
 }
